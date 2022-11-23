49af69ffd09ba395320d67da319390c0ac958b79
Duy Tran <duyvutran2303@gmail.com>
2022-11-23 14:37:34 -0500

    fix: add model parameter to evaluate

--- src/evaluate.py
+++ src/evaluate.py
@@ -2,7 +2,8 @@ from numpy import dot
 from numpy.linalg import norm
 import datetime
 import pandas as pd
-from feature_extract import PATH_QUERY_FILE, extract_features_query_file
+from feature_extract import PATH_QUERY_FILE, extract_features_query_file, get_model
+from sentence_transformers import SentenceTransformer
 
 PATH_RESULTS_SAVE = './results/scored_query_file' + \
     datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + '.jsonl'
@@ -12,10 +13,10 @@ PATH_RESULTS_SAVE = './results/scored_query_file' + \
 # and its candidate vector
 
 
-def evaluate(path: str):
+def evaluate(path: str, model: SentenceTransformer):
     query_df = pd.read_json(path, lines=True)
     query_df_scored = query_df.copy(deep=True)
-    query_file_feature_vectors = extract_features_query_file(path)
+    query_file_feature_vectors = extract_features_query_file(path, model)
     assert (len(query_file_feature_vectors) == len(query_df))
 
     # TODO speed this up with matrix-vector multiplication?
@@ -33,5 +34,6 @@ def evaluate(path: str):
 
 if __name__ == "__main__":
     # query_df = pd.read_json(PATH_QUERY_FILE, lines=True)
-    scored = evaluate(PATH_QUERY_FILE)
+    model = get_model()
+    scored = evaluate(PATH_QUERY_FILE, model)
     scored.to_json(path_or_buf=PATH_RESULTS_SAVE, orient='records', lines=True)
